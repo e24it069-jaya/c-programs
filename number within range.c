d4e0f830363edcab84d8a15bfd50210f7b042017
e24it069-jaya <e24it069@shanmugha.edu.in>
2026-02-11 13:39:33 +0530

    Add files via upload

---
+++ number within range.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(10<n && n<20)
+        printf("Number is within range");
+    else
+        printf("Number is not within range");
+    return 0;
+}
