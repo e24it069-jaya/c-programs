7256b250f9b9efc9072de401c53b05151657df85
e24it069-jaya <e24it069@shanmugha.edu.in>
2026-02-11 16:24:49 +0530

    Add files via upload

---
+++ digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch>='A'&&ch<='Z'||ch>='a'&&ch<='z')
+        printf("Alphabet");
+    else if(ch<='9'&& ch>='0')
+        printf("Digit");
+    else
+        printf("Special character");
+    
+    return 0;
+}
